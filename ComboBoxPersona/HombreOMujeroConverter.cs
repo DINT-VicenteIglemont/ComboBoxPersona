38a051179e98c02fadba29bb4ee44aa006b91837
Vicente Iglesias Montoya <vicente.iglesias@iesdoctorbalmis.com>
2019-11-15 09:13:12 +0100

    Agregar archivos de proyecto.

---
+++ ComboBoxPersona/HombreOMujeroConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ComboBoxPersona
+{
+    class HombreOMujeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value.ToString().ToLower())
+            {
+                case "mujer":
+                    return "mujer.png";
+                case "hombre":
+                    return "hombre.png";
+                    
+            }
+
+            return null;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
